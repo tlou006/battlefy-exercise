bd173866ff17959ccf19a6c06ca4ea73a69e85d7
illpot <user@server.com>
2019-02-11 09:19:40 -0800

    kayn setup

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-
+process.env.LEAGUE_API_KEY = "RGAPI-950201d9-4efc-4288-9fcc-cb2923f98425";
 
 const Express = require("express");
 const app = Express();
+const { Kayn, REGIONS } = require("kayn");
+const KaynClient = Kayn(process.env.LEAGUE_API_KEY)();
+
+app.get("/", async (req, res, next) => {
+    const summoner = await KaynClient.Summoner.by.name(req.query.name);
+    res.json(summoner);
+    //res.send("Battlefy exercise app API");
+});
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
